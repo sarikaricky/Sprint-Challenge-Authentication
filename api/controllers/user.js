d88feffd1dcb2100d9d921a4283071be91244ea7
sarika <sarikaresume@gmail.com>
2018-01-19 15:09:35 -0800

    stretch

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,7 +4,25 @@ const bcrypt = require('bcrypt');
 const createUser = (req, res) => {
   // there should be a user object set on req
   // use that req.user object to create a user and save it to our Mongo instance.
-};  db.createUser() 
+};  
+const newUser = new User(req.body.user)
+
+newUser.name = tony(newUser.name)
+newUser.save((err, saved) => {
+  if (err) {
+    res.status(500).send(err)
+  }
+  res.json({ user: saved })
+})
+}
+
+  Tony.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+    res.json({ user: saved })
+  })
+}
 
 module.exports = {
   createUser
